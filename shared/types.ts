cdbe9dcd0db44c2291a972ce260072ec5a752afd
hamed ajaj <hamedsgaming@gmail.com>
2025-09-21 16:05:41 +0300

    hono error handling

---
+++ shared/types.ts
@@ -0,0 +1,13 @@
+export type SuccessResponse<T = void> = {
+  success: true;
+  message: string;
+} & (T extends void ? {} : { data: T });
+
+export type ErrorResponse = {
+  success: false;
+  error: string;
+  isFormError?: boolean;
+}
+
+
+
